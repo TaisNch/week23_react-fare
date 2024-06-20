ca663d330703e6e4eb0a8b7a3818eb180f49980e
TaisNch <tais.aliexpress@gmail.com>
2024-06-20 10:34:30 +0200

    commit2006

--- fare-rates/src/App.js
+++ fare-rates/src/App.js
@@ -1,4 +1,5 @@
 import Card from './components/card';
+import './App.css';
 import style300 from './components/rate300.modules.css';
 import style450 from './components/rate450.modules.css';
 import style550 from './components/rate550.modules.css';
@@ -11,29 +12,29 @@ function App() {
     <Card
     style= {style300}
     name="300"
-    price="300"
-    speed= "100"
+    price={300}
+    speed= {10}
     traffic = 'Объем включенного трафика не ограничен'
     />
     <Card
     style= {style450}
     name="450"
-    price="450"
-    speed= "50"
+    price={450}
+    speed= {50}
     traffic = 'Объем включенного трафика не ограничен' 
     />
     <Card
     style= {style550}
     name="550"
-    price="550"
-    speed= "550"
+    price={550}
+    speed= {100}
     traffic =  'Объем включенного трафика не ограничен'
     />
     <Card
     style= {style1000}
     name="1000"
-    price="1000"
-    speed= "200"
+    price={1000}
+    speed= {200}
     traffic =  'Объем включенного трафика не ограничен'
     />
   </div>
--- fare-rates/src/components/card.jsx
+++ fare-rates/src/components/card.jsx
@@ -2,18 +2,19 @@
 
 function Card(props) {
     const style = props.style;
-    const card =  `${style.card} rate_card `;
-    let rate
-    Number(props.rate === 550)
-    ? (rate = 'card increase')
-    : (rate = 'card');
+    const name =  `${style.card} name_card`;
+    const price =  `${style.card} price_card`;
+    let card
+   Number(props.price === 550)
+    ? (card = 'card increase')
+    : (card = 'card')
  return (
         <div className={card}>
-         <div className="name">{props.name}<span> Безлимитный </span>
-         </div>
+           <div className="name">{props.name}<span> Безлимитный </span>
             <div className="price">
-                {props.price} <span> руб/мес</span>
+                {props.card} <span className="price_box"> руб/мес</span>
             </div>
+         </div>
             <div className="speed">до {props.speed} Мбит/сек</div>
             <div className="traffic">{props.traffic}</div>
         </div>
